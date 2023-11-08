f0c34e49b2d57dd2e41732532b1209a9a2408833
Ewokkkkk <ewok.mfk@gmail.com>
2023-11-08 01:26:18 +0000

    modified function

--- get_co2.py
+++ get_co2.py
@@ -1,14 +1,18 @@
 import mh_z19
+import time
+import timeout_decorator
 
-
+@timeout_decorator.timeout(10)
 def read():
-    out = mh_z19.read()
-    return str(out)[8:-1]
+    value = ""
+    while value == "":
+        out = mh_z19.read()
+        value = str(out)[8:-1]
+    
+    return value
 
 
 if __name__ == '__main__':
     co2 = read()
-    while co2 == "":
-        co2=read()
 
     print(co2)
